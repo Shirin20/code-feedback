1a2e2245c0d9c25ce9ccb2409093d8a362bac265
Shirin Meirkhan <sm223qi@student.lnu.se>
2022-10-15 03:19:54 +0200

    Change files names

--- src/ProgramActions.js
+++ src/ProgramActions.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsdoc/require-jsdoc */
 import shell from 'shelljs'
 import fs from 'fs'
+import { resolve } from 'path'
 import { ProjectCodeChecker } from 'code-statistics/src/ProjectCodeChecker.js'
 import { ProjectFilesReader } from 'code-statistics/src/ProjectFilesReader.js'
 
 const projectFiles = new ProjectCodeChecker()
 const projectReader = new ProjectFilesReader()
 
-export class UserActions {
+export class ProgramActions {
   cloneProject (url) {
     const path = 'students-projects'
     shell.cd(path)
@@ -26,7 +27,6 @@ export class UserActions {
 
   async report (projectRootPath) {
     const projectFilesArray = await projectReader.getDirectoryFilesPaths(projectRootPath)
-    console.log(projectFilesArray)
     const reportObj = {
       projectLines: await projectFiles.countProjectLines(projectFilesArray),
       projectIfStatements: await projectFiles.countProjectIfStatements(projectFilesArray),
@@ -34,5 +34,17 @@ export class UserActions {
       projectsCharacters: await projectFiles.countProjectCharacters(projectFilesArray)
     }
     return reportObj
+  }
+
+  async writePersistentFeedback (feedbackArray) {
+    const dataSource = resolve(process.cwd(), 'students-reports-feedback.text')
+    feedbackArray.shift()
+    const feedback = feedbackArray.join(' ')
+    const data = JSON.stringify(feedback, null, 4)
+    return fs.appendFile(dataSource, `
+    ${data}`, function (err) {
+      if (err) throw err
+      console.log('Saved!')
+    })
   }
 }
