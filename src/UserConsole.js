606db1e6fae4236112ddbaf4c267e31a55572b4a
Shirin Meirkhan <sm223qi@student.lnu.se>
2022-10-15 03:17:36 +0200

    Delete the package inquirer, it did not work with code statistics

--- src/UserConsole.js
+++ src/UserConsole.js
@@ -1,81 +1,38 @@
 /* eslint-disable promise/param-names */
 /* eslint-disable jsdoc/require-jsdoc */
-import chalk from 'chalk'
-import inquirer from 'inquirer'
-import gradient from 'gradient-string'
-import chalkAnimation from 'chalk-animation'
-import figlet from 'figlet'
-import { createSpinner } from 'nanospinner'
 
 export class UserConsole {
-  async welcome () {
-    const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms))
-    const rainbowTitle = chalkAnimation.rainbow(
-      '                    Student project reports \n'
-    )
-
-    await sleep()
-    rainbowTitle.stop()
-
+  showMenu () {
     console.log(`
-    ${chalk.bgBlue('How to use the program')} 
-    Start by entering the name of the student and the name of the project
-    check a report on the project code 
-    write a feedback to the student 
-    Then student name, project name, report and feedback
-    will be saved in the ${chalk.bgRed('report.text')} file
- `)
+    ***************** Student project reports **********
+    menu, help - show this menu
+  
+    clone <gitHub url> -  To clone a student project
+  
+    report - report <project dir root path>
+  
+    feedback <student name, project name, feedback text> - to write a feedback
+    Write the student name and the project name separated by ' , ' and then write the feedback 
+  
+    delete- <project dir name> To delete a project
+    
+    exit, quit - Exits Program
+    `)
   }
 
-  async askName () {
-    const answers = await inquirer.prompt({
-      name: 'student_name',
-      type: 'input',
-      message: 'Student full name?',
-      default () {
-        return 'firstName lastName'
-      }
-    })
-    const studentName = answers.student_name
-    return studentName
+  printReport (reportObject) {
+    console.log(`
+    project Lines = ${reportObject.projectLines}
+    project if statements = ${reportObject.projectIfStatements}
+    project for loops = ${reportObject.projectLinesForLoops}
+    project projectsCharacters and whitespace = ${reportObject.projectsCharacters}
+    `)
   }
 
-  async askProjectName () {
-    const answers = await inquirer.prompt({
-      name: 'project_name',
-      type: 'input',
-      message: 'project?',
-      default () {
-        return 'projectName'
-      }
-    })
-    const projectName = answers.project_name
-    return projectName
-  }
+  exitProgram (code) {
+    code = code || 0
 
-  async askGitHubUrl () {
-    const answers = await inquirer.prompt({
-      name: 'github_url',
-      type: 'input',
-      message: 'Please write the git hup url to clone the project',
-      default () {
-        return 'Github url'
-      }
-    })
-    const gitHubUrl = answers.github_url
-    return gitHubUrl
-  }
-
-  async askIfToPrintReport () {
-    const answers = await inquirer.prompt({
-      name: 'print_report',
-      type: 'confirm',
-      message: 'do you want to print the report',
-      default () {
-        return 'Y/N'
-      }
-    })
-    const printReport = answers.print_report
-    return printReport
+    console.info('\nExiting with exit status: ' + code)
+    process.exit(code)
   }
 }
